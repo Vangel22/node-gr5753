9b51e20b61156065d9bcb47a7a34c60d3f4c5dc8
Vangel22 <h.vangel22@gmail.com>
2026-03-17 21:54:28 +0100

    Save class 07

---
+++ c07/v2/handler/students.js
@@ -0,0 +1,63 @@
+const { read, write } = require("../read-write");
+
+// Create Read Update Delete
+// POST GET PUT DELETE -> HTTP metodi
+
+const getAllStudents = async (req, res) => {
+  const students = await read();
+
+  return res.status(200).send(students);
+};
+
+const addStudent = async (req, res) => {
+  // req.body
+  const students = await read();
+  students.push(req.body);
+  await write(students);
+
+  return res.status(200).send("New student added!");
+};
+
+const deleteStudent = async (req, res) => {
+  // req.params.studentIndex -> String
+  let students = await read();
+  const studentIndex = Number(req.params.studentIndex);
+  // studentIndex = "1" -> 1
+
+  // index e brojka, a req.params.studentIndex e String
+  students = students.filter((student, index) => index !== studentIndex);
+  await write(students);
+
+  return res
+    .status(200)
+    .send(`Student with index - ${studentIndex} deleted successfully!`);
+};
+
+const editStudent = async (req, res) => {
+  const studentIndex = Number(req.params.studentIndex);
+  const studentData = req.body;
+
+  let students = await read();
+  students = students.map((student, index) => {
+    if (index === studentIndex) {
+      return {
+        ...student,
+        ...studentData,
+      };
+    }
+    return student;
+  });
+
+  await write(students);
+
+  return res
+    .status(200)
+    .send(`Student with index - ${studentIndex} has been edited!`);
+};
+
+module.exports = {
+  getAllStudents,
+  addStudent,
+  deleteStudent,
+  editStudent,
+};
---
+++ c07/v2/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const {
+  getAllStudents,
+  addStudent,
+  editStudent,
+  deleteStudent,
+} = require("./handler/students");
+
+const app = express();
+// express() vrakja:
+// return {
+//     get: () => {}
+//     post: () => {}
+// }
+
+// middleware - presretnuvac
+app.use(express.json()); // ke ni ovozmozi req.body da bide od tip JSON
+// ako go nemate ovoj del, vo data.json koga ke dodadete student ke vi izleze null podatok:
+
+// [
+//   { "ime": "Vangel", "prezime": "Hristov", "godina": 1999 },
+//   { "ime": "Pero", "prezime": "Perov", "godina": 2005, "univerzitet": "FINKI" }
+//    null
+// ]
+
+// if(url.pathname === "/students" && req.method === "GET"){}
+app.get("/students", getAllStudents);
+app.post("/students", addStudent);
+app.delete("/students/:studentIndex", deleteStudent);
+app.put("/students/:studentIndex", editStudent);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started at port ${port}!`);
+});
